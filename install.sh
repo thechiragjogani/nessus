fc551896851454f314ae5c86301a3ce9f356ac4d
Chirag Jogani <chiragjoganicyber@gmail.com>
2023-07-13 23:38:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ sudo cat > /usr/bin/nessus<<'EOF'
 #!/bin/bash
 cd $HOME
 vernum=`curl https://plugins.nessus.org/v2/plugins.php 2> /dev/null`
-installedPlugins=`cat /opt/nessus/var/nessus/plugin_feed_info.inc | grep 2 | cut -b 15-26`
+installedPlugins=`cat /opt/nessus/var/nessus/plugin_feed_info.inc &> /dev/null | grep 2 | cut -b 15-26`
 echo " o Checking for new plugins."
 if [ "$installedPlugins" = "$vernum" ]; then
    echo
