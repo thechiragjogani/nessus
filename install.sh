b726851ef31abcbdc58e94693d883f0e8a0f9fa3
Chirag Jogani <chirag.joganibca2021052@imsnoida.com>
2024-04-08 19:49:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 cd $HOME
-sudo apt install expect -y
 exec 2>/dev/null
 sudo rm Nessus-* 2>/dev/null
 sudo rm all-2.0* 2>/dev/null
